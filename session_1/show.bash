08fe6681e4b782aa408852533e934d7b680d8fac
Jay ZHAO <jayzhao0115@163.com>
2024-09-19 19:58:52 +0800

    modified:   session_1/show.bash

--- session_1/show.bash
+++ session_1/show.bash
@@ -3,12 +3,12 @@ cd ~/ws_4202
 . session_1/install/setup.bash
 
 echo "========== 1. Starting Simulator =========="
-ros2 run turtlesim turtlesim_node & sleep 6;
+ros2 run turtlesim turtlesim_node & sleep 3;
 
 echo "========== 2. Starting turtle circle controller =========="
-ros2 run turtle_control turtle_control_node & sleep 5;
+ros2 run turtle_control turtle_control_node & sleep 3;
 
-echo "========== 2. Starting turtle pose show =========="
-ros2 run turtle_control turtle_pose_show_node & sleep 5;
+echo "========== 3. Starting turtle pose show =========="
+ros2 run turtle_control turtle_pose_show_node & sleep 3;
 
 wait;
