ed00a9a1d23e7707aecbfb3a7c931b4d3a224222
Jay ZHAO <jayzhao0115@163.com>
2024-09-26 21:00:32 +0800

    Modified comments

--- session_1/src/turtle_control/turtle_control/turtle_control_node.py
+++ session_1/src/turtle_control/turtle_control/turtle_control_node.py
@@ -8,14 +8,15 @@ class TurtleControlNode(Node):
         self.publisher_ = self.create_publisher(Twist, '/turtle1/cmd_vel', 10)
         self.timer = self.create_timer(0.5, self.publish_velocity)  # 2 Hz
         self.radius = 2.0
-        self.angular_speed = 0.5  # 设置角速度为0.5 rad/s
+        self.angular_speed = 0.5  # Set angular velocity as 0.5 rad/s
         self.linear_speed = self.radius * self.angular_speed
 
     def publish_velocity(self):
         msg = Twist()
-        msg.linear.x = self.linear_speed  # 线速度
-        msg.angular.z = self.angular_speed  # 角速度
+        msg.linear.x = self.linear_speed  # Linear velocity
+        msg.angular.z = self.angular_speed  # Angular velocity
         self.publisher_.publish(msg)
+        #Test
         # self.get_logger().info(f'Publishing velocity: linear={self.linear_speed}, angular={self.angular_speed}')
 
 def main(args=None):
